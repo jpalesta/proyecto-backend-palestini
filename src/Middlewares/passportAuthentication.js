6d2a2b0a756c0e4c87e82082c51adf4a7cfeadd8
jpalesta <109118478+jpalesta@users.noreply.github.com>
2023-06-25 17:59:23 -0300

    login github ok

--- src/Middlewares/passportAuthentication.js
+++ src/Middlewares/passportAuthentication.js
@@ -5,7 +5,7 @@ const passportAuthentication = () => {
         passport.authenticate('jwt',{session:false}, function(err, user, info) {
             if(err) return next(err)
             if(!user) {
-                return res.status(401).send({ status: 'errore', error: info.message ? info.message : info.toString() })
+                return res.status(401).send({ status: 'error', error: info.message ? info.message : info.toString() })
             }
             req.user = user
             next()
