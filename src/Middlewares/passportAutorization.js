ddd48dbf529827c2d24f9df14d0f64a64d1f8148
jpalesta <jose.palestini@gmail.com>
2023-07-26 20:03:23 -0300

    1° commit

--- src/Middlewares/passportAutorization.js
+++ src/Middlewares/passportAutorization.js
@@ -1,13 +1,18 @@
-const passportAutorization = roles => {
-    return async (req, res, next)=> {
-        if(!req.user.user) return res.status(401).send({status: 'error', error: 'Unauthorized'})
-        if(!roles.includes(req.user.user.role)) {
-            return res.status(403).send({status: 'error', error: 'Not permissions'})
+const passportAutorization = (roles) => {
+    return async (req, res, next) => {
+        if (!req.user.user)
+            return res
+                .status(401)
+                .send({ status: 'error', error: 'Unauthorized' })
+        if (!roles.includes(req.user.user.role)) {
+            return res
+                .status(403)
+                .send({ status: 'error', error: 'Not permissions' })
         }
         next()
     }
 }
 
 module.exports = {
-    passportAutorization
+    passportAutorization,
 }
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -1,20 +1,23 @@
 const { Router } = require('express')
 const router = Router()
 
-const {getAll, 
-    getOne, 
-    create, 
-    update, 
-    deleteAll, 
-    updateProduct, 
-    deleteProduct, 
+const {
+    getAll,
+    getOne,
+    create,
+    update,
+    deleteAll,
+    updateProduct,
+    deleteProduct,
     updateProductQuantity,
-    purchase
-} = require ('../controllers/carts.controller')
+    purchase,
+} = require('../controllers/carts.controller')
 
 const { isAuthenticated } = require('../Middlewares/authentication.middlewares')
 const { passportAutorization } = require('../Middlewares/passportAutorization')
-const { passportAuthentication } = require('../Middlewares/passportAuthentication')
+const {
+    passportAuthentication,
+} = require('../Middlewares/passportAuthentication')
 
 //Consulta de todos los corritos
 router.get('/', getAll)
@@ -25,10 +28,15 @@ router.get('/:cid', getOne)
 //Crea un carrito con productos pasados por body con validación
 router.post('/', create)
 
-//Agrega o incrementa un producto en un carrito, ambos pasan por params 
-router.post('/:cid/products/:pid', isAuthenticated, passportAutorization('user'), update)
+//Agrega o incrementa un producto en un carrito, ambos pasan por params
+router.post(
+    '/:cid/products/:pid',
+    isAuthenticated,
+    passportAutorization(['admin', 'premium']),
+    update
+)
 
-//Vacía un carrito por id 
+//Vacía un carrito por id
 router.delete('/:cid', deleteAll)
 
 //Borra un producto de un carrito por id de carrito e id de producto
@@ -40,99 +48,6 @@ router.put('/:cid', updateProduct)
 //actualiza la cantidad de un producto de un carrito por id con cantidad por body falta validar body
 router.put('/:cid/products/:pid', updateProductQuantity)
 
-router.post('/:cid/purchase', passportAuthentication('jwt'),purchase)
+router.post('/:cid/purchase', passportAuthentication('jwt'), purchase)
 
 module.exports = router
-
-//LÓGICA FILES
-// const CartManager = require('../dao/fileSystem/cartManager')
-// const ProductManager = require('../dao/fileSystem/productManager')
-
-// const cart = new CartManager()
-// const product = new ProductManager()
-
-// //chequeado OK
-// router.post('/', async (req, res) => {
-//     try {
-//         await cart.addCarts()
-//         let newCartArray = await cart.getCarts()
-//         res.send({
-//             status: 'success',
-//             message: 'carrito agregado correctamente',
-//             payload: newCartArray
-//         })
-//     } catch (error) {
-//         res.status(400).send({
-//             status: 'error',
-//             message: error
-//         })
-//     }
-// })
-// //chequeado OK
-// router.get('/:cid', async (req, res) => {
-//     try {
-//         const { cid } = req.params
-//         const cartById = await cart.getCartsById(parseInt(cid))
-//         if (!cartById) {
-//             return res.send({
-//                 status: 'error',
-//                 error: 'cart not found'
-//             })
-//         }
-//         res.send({
-//             status: 'success',
-//             payload: cartById
-//         })
-//     } catch (error) {
-//         return res.send({
-//             status: 'error',
-//             error: 'cart not found'
-//         })
-//     }
-// })
-// //chequeado OK
-// router.post('/:cid/products/:pid', async (req, res) => {
-//     try {
-//         const { cid, pid } = req.params
-//         const cartById = await cart.getCartsById(parseInt(cid))
-//         if (!cartById) {
-//             return res.send({
-//                 status: 'error',
-//                 error: 'cart not found'
-//             })
-//         } else {
-//             const productById = await product.getProductsById(parseInt(pid))
-//             if (!productById) {
-//                 return res.status(400).send({
-//                     status: 'error',
-//                     message: 'Product not found'
-//                 })
-//             } else {
-//                 let productToAdd = cartById.products.find(prod => prod.id === parseInt(pid))
-//                 if (productToAdd) {
-//                     productToAdd.quantity++
-//                     await cart.updateCart(cid, cartById)
-//                     res.status(200).send({
-//                         status: 'success',
-//                         message: 'product increased +1 ok',
-//                         payload: cartById
-//                     })
-//                 } else {
-//                     productToAdd = { id: parseInt(pid), quantity: 1 }
-//                     cartById.products.push(productToAdd)
-//                     await cart.updateCart(cid, cartById)
-//                     res.status(200).send({
-//                         status: 'success',
-//                         message: 'product added ok',
-//                         payload: cartById
-//                     })
-//                 }
-//             }
-//         }
-//     } catch (error) {
-//         return res.status(400).send({
-//             status: 'error',
-//             error: 'cart or product not found'
-//         })
-//     }
-// })
