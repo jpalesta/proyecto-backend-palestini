468a327ccc527822455e537a0adaf45e1609c5fe
jpalesta <jose.palestini@gmail.com>
2023-09-03 09:46:51 -0300

    modificacion en CORS

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(addlogger)
 
 //configuracion de CORS
 const corsOptions = {
-    origin: 'http://localhost:8080',
+    origin: '*',
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization'
 }
